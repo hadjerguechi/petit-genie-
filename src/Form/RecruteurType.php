4da814407229000e6df45fe8deb3279d52607628
Dev <Dev@MacBook-Pro-de-Geoffrey.local>
2021-06-08 14:32:51 +0200

    new

---
+++ src/Form/RecruteurType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recruteur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecruteurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('companyname')
+            ->add('description')
+            ->add('contactname')
+            ->add('contactemail')
+            ->add('phonenumber')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Recruteur::class,
+        ]);
+    }
+}
