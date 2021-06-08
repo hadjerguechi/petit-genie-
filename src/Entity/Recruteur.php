c1d2cdc1dd2d1262572709408f0aec182db0868f
Dev <Dev@MacBook-Pro-de-Geoffrey.local>
2021-06-08 13:58:55 +0200

    ajout entity

--- src/Entity/Recruteur.php
+++ src/Entity/Recruteur.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Entity;
-
+use Symfony\Component\Validator\Constraints as Assert;
 use App\Repository\RecruteurRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * 
  * @ORM\Entity(repositoryClass=RecruteurRepository::class)
  */
 class Recruteur
@@ -17,25 +18,73 @@ class Recruteur
      */
     private $id;
 
-    /** //asserts... min 2 max 80 
+    /**
      * @ORM\Column(type="string", length=80)
+     * @Assert\NotBlank(message = "Merci de renseigner le nom de l'entreprise")
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 100,
+     *      minMessage = "Le nom de l'entreprise ne peut être inferieur à 2 caractères",
+     *      maxMessage = "Le nom de l'entreprise ne peut être superieur à 100 caractères",
+     * )
      */
+
     private $companyname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message = "Merci de renseigner la description de l'entreprise")
+     * @Assert\Length(
+     *      min = 20,
+     *      max = 255,
+     *      minMessage = "La description de l'entreprise ne peut être inferieur à 20 caractères",
+     *      maxMessage = "La description de l'entreprise ne peut être superieur à 255 caractères",
+     * )
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message = "Merci de renseigner le nom du contact au sein de l'entreprise")
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 100,
+     *      minMessage = "La description de l'entreprise ne peut être inferieur à 2 caractères",
+     *      maxMessage = "La description de l'entreprise ne peut être superieur à 100 caractères",
+     * )
      */
     private $contactname;
 
     /**
      * @ORM\Column(type="string", length=100)
+     * @Assert\Email(
+     *     message = "L'e-mail '{{ value }}' n'est pas valide."
+     * )
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 100,
+     *      minMessage = "L'e-mail ne peut être inferieur à 5 caractères",
+     *      maxMessage = "L'e-mail ne peut être superieur à 100 caractères",
+     * )
+     * @Assert\NotBlank(message = "Merci de renseigner un e-mail")
      */
     private $contactemail;
+
+    /**
+     * @ORM\Column(type="string", length=20)     
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 20,
+     *      minMessage = "Le numero de téléphone ne peut être inferieur à 10 caractères",
+     *      maxMessage = "Le numero de téléphone ne peut être superieur à 20 caractères",
+     * )
+     * @Assert\Regex(
+     *       pattern="/^\(0\)[0-9]*$", 
+     *       message="Format invalide"
+     * )
+     * @Assert\NotBlank(message = "Merci de renseigner un numéro de téléphone")
+     */
+    private $phonenumber;
 
     public function getId(): ?int
     {
@@ -86,6 +135,18 @@ class Recruteur
     public function setContactemail(string $contactemail): self
     {
         $this->contactemail = $contactemail;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?string
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(string $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
 
         return $this;
     }
